989e4cdf5311965cdfc360db30eb2919ec4ab1b6
Krzysztof Jura <kjura@vewd.com>
2018-01-02 15:54:48 +0100

    updated '5. TCP multi client chat/server select based.py' to not use threading

--- socket programming/5. TCP multi client chat/server select based.py
+++ socket programming/5. TCP multi client chat/server select based.py
@@ -1,17 +1,9 @@
 #!/usr/bin/env python3
 import select
 import socket
-import threading
 
 from shared.config import HOST, PORT
 from shared.server import setup, handle_packets, finish, clients, connection_to_client_map
-
-
-def new_client_listener():
-    while True:
-        connection, client_address = server.accept()
-        setup(connection=connection, client_address=client_address)
-
 
 if __name__ == "__main__":
     server = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
@@ -19,12 +11,13 @@ if __name__ == "__main__":
         server.bind((HOST, PORT))
         server.listen(100)
 
-        thread = threading.Thread(target=new_client_listener)
-        thread.start()
-
         while True:
-            rlist, wlist, xlist = select.select(connection_to_client_map, [], [], 0.03)
+            rlist, wlist, xlist = select.select(list(connection_to_client_map) + [server], [], [], 0.03)
 
-            for connection in rlist:
-                if not handle_packets(connection=connection):
-                    finish(connection=connection)
+            for rconn in rlist:
+                if rconn is server:
+                    connection, client_address = server.accept()
+                    setup(connection=connection, client_address=client_address)
+                elif not handle_packets(connection=rconn):
+                    finish(connection=rconn)
+
